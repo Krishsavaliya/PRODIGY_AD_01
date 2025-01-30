a0b73601851ab9ac5f20cf73cafa74774dacb09b
Krishsavaliya <krishsavaliya17103@gmail.com>
2025-01-30 11:20:13 +0530

    Almost finished Version

--- app/src/main/java/com/example/simplecalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculatorapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.simplecalculatorapp.ui.theme.MediumGray
 import com.example.simplecalculatorapp.ui.theme.SimpleCalculatorAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +23,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleCalculatorAppTheme {
-
+                val viewModel = viewModels<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
